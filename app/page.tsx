6743dde25b798e9cddb3116e87b8adc979841c62
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-13 11:58:56 +0530

    Added all the initial routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl italic'>Welcome to my saas app</h1>
+      <Button>Click Me</Button>
+    </div>
   )
 }
 
