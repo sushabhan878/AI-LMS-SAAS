41c41fbfa6c4a08e929f4bc7c8c9389cda8b9fc3
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-20 10:33:04 +0530

    Added utils.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -22,7 +23,7 @@ const Page = () => {
           topic="Science"
           subject="English"
           duration={45}
-          color="##e5d0ff"
+          color="#e5d0ff"
         />
         <CompanionCard
           id="678"
@@ -34,7 +35,11 @@ const Page = () => {
         />
       </section>
       <section className='home-section'>
-        <CompanionsList />
+        <CompanionsList
+          title="Recently compleated Sessions"
+          companions={recentSessions}
+          className="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
     </main>
--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const CompanionsList = () => {
+interface companionsListProps {
+    title: string,
+    companions?: Companion[]
+    className?: string
+}
+const CompanionsList = ({ title, companions, className }: companionsListProps) => {
     return (
-        <div>
-            Companions List
-        </div>
+        <article className={cn("companion-list", className)}>
+            <h2 className='font-bold text-3xl'>Recent Sessions</h2>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-lg w-2/3">Lessons</TableHead>
+                        <TableHead className='text-lg '>Subject</TableHead>
+                        <TableHead className='text-lg text-right'>Duration</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {companions?.map((companion) => (
+                        <TableRow key={companion.id}>
+                            <TableCell>
+                                <Link href={`/companions/${companion.id}`}>
+                                    <div className='flex items-center gap-2'>
+                                        <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden'>
+                                            <Image src={`/icons/${companion.subject}.svg`} alt="subject" width={35} height={35} />
+                                        </div>
+                                    </div>
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </article>
     )
 }
 
