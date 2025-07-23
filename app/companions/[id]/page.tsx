852ff851414556a193af83d06529518e7c11cb75
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-23 19:06:54 +0530

    Added new companion redirect page

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CompanionSession = () => {
     return (
         <div>
-
+            <h1>Companion Session</h1>
         </div>
     )
 }
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -25,6 +25,8 @@ import {
 } from "@/components/ui/select"
 import { subjects } from "@/constants"
 import { Textarea } from "./ui/textarea"
+import { createCompanion } from "@/lib/actions/companion.action"
+import { redirect } from "next/navigation"
 const formSchema = z.object({
     name: z.string().min(1, { message: "Companion is required." }),
     subject: z.string().min(1, { message: "Subject is required." }),
@@ -47,8 +49,14 @@ const CompanionForm = () => {
             duration: 15
         },
     })
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const companion = await createCompanion(values)
+        if (companion) {
+            redirect(`/companions/${companion.id}`)
+        } else {
+            console.log("Failed to create a companion")
+            redirect("/")
+        }
     }
 
     return (
