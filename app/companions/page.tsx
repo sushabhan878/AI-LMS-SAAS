4485c555ba6304c6ab96cf740affa771c7f249c3
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-24 19:51:08 +0530

    Added the companion card

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,34 @@
+
+import CompanionCard from '@/components/CompanionCard';
+import { getAllCompanions } from '@/lib/actions/companion.action'
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const CompanionLibrary = () => {
+const CompanionLibrary = async ({ searchParams }: SearchParams) => {
+    const filteres = await searchParams;
+    const subject = filteres?.subject ? filteres.subjects : "";
+    const topic = filteres?.topic ? filteres.topic : "";
+
+    const companions = await getAllCompanions({
+        subject: typeof subject === 'string' ? subject : undefined,
+        topic: typeof topic === 'string' ? topic : undefined
+    })
+
+    console.log(companions)
     return (
-        <div>
-            <h1>Companion Librery</h1>
-        </div>
+        <main className=''>
+            <section className='flex justify-between gap-4 max-sm:flex--col'>
+                <h1>Companion library</h1>
+                <div className='flex gap-4'>
+                    Filters
+                </div>
+            </section>
+            <section className='companions-grid'>
+                {companions.map((companion) => (
+                    <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+                ))}
+            </section>
+        </main>
     )
 }
 
