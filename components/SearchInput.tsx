d4fd98adea9a04b5a48743f0d683e50bb704247c
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-24 21:06:48 +0530

    Compleated inmplimentation of search input feature

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,9 @@
 "use client"
 
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils'
 import Image from 'next/image'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const SearchInput = () => {
     const pathname = usePathname()
@@ -10,9 +11,31 @@ const SearchInput = () => {
     const searchParams = useSearchParams()
 
     const query = searchParams.get("topic") || ""
-
-
     const [searchQuery, setSearchQuery] = useState("")
+
+
+    // Changing the url in urlBar
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if (searchQuery) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "topic",
+                    value: searchQuery
+                })
+                router.push(newUrl, { scroll: false })
+            } else {
+                if (pathname === "/companions") {
+                    const newUrl = removeKeysFromUrlQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ["topic"]
+                    })
+                    router.push(newUrl, { scroll: false })
+                }
+            }
+        }, 500);
+
+    }, [searchQuery, router, searchParams, pathname])
 
     return (
         <div className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit'>
