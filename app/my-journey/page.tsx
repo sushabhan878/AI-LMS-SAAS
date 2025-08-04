a0e85749a6f3732d62b2c26e06a532915d4a5e71
Sushabhan <sushabhanmajumder878@gmail.com>
2025-08-04 09:41:18 +0530

    Done withe the project

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
-const Profile = () => {
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { getRecentSessions, getUserCompanions } from '@/lib/actions/companion.action'
+const Profile = async () => {
+    const user = await currentUser()
+    if(!user) redirect('/sign-in')
+    const companions = await getUserCompanions(user.id)
+    const sessionHistory = await getRecentSessions(user.id)
     return (
-        <div>
-
-        </div>
+        <main className='min-lg:w-3/4'>
+            <section>
+                <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+                </Accordion>
+            </section>
+        </main>
     )
 }
 
--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -68,10 +68,22 @@ export const getRecentSessions = async ({ limit = 10 }: { limit?: number }) => {
 
 export const getUserSessions = async (userId: string, limit = 10) => {
     const supabase = createSupabaseClient()
-    const {data, error} = await supabase.from("session_history").select("companions:companion_id (*)").eq("user_id", userId).order("created_at", {ascending: false}).limit(limit)
+    const {data, error} = await supabase.from("session_history").select("companions:copanion_id (*)").eq("user_id", userId).order("created_at", {ascending: false}).limit(limit)
 
     if(error) throw new Error(error.message)
     return data.map(({ companions }) => (
         companions
     ))
+}
+
+export const getUserCompanions = async (userId: string) => {
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('author', userId)
+
+    if(error) throw new Error(error.message);
+
+    return data;
 }
